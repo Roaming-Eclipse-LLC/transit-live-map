be8536f15c473ab9571a0ffa2672be267e7fe078
Astrid Sutton <11416752+laarniastrid@users.noreply.github.com>
2026-07-23 13:50:02 -0500

    feat: add cookie consent banner with overlay and deferred vehicle rendering (#79)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { NavButton } from './components/NavButton';
+import { CookieBanner } from './components/CookieBanner';
 
 import { MapPage } from './pages/MapPage';
 import { TechStackPage } from './pages/TechStackPage';
 
+const CONSENT_KEY = 'cookieConsent';
+
 const App = () => {
+  const [consented, setConsented] = useState(
+    () => !!localStorage.getItem(CONSENT_KEY),
+  );
+
   return (
     <>
       <NavButton />
       <Routes>
-        <Route path='/' element={<MapPage />} />
+        <Route
+          path='/'
+          element={
+            <MapPage hideLoadingOverlay={!consented} consented={consented} />
+          }
+        />
         <Route path='/tech-stack' element={<TechStackPage />} />
       </Routes>
+
+      {!consented && <CookieBanner onAccept={() => setConsented(true)} />}
     </>
   );
 };
